5e08041da104854f1254fbe200fab4344c29f323
xunuosi <xunuosi@163.com>
2016-10-14 15:55:01 +0800

    Commit 添加工具类MFGT（用于界面切换的工具）和L(控制台输出的控制工具类)

---
+++ app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
@@ -0,0 +1,39 @@
+package cn.ucai.fulicenter.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.activity.MainActivity;
+
+
+public class MFGT {
+    /**
+     * 补间动画关闭当前activity
+     * @param activity
+     */
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    /**
+     * 跳转开启的界面
+     * @param context
+     */
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+
+    /**
+     * 重写开启界面的方法
+     * @param context
+     * @param cls
+     */
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+}

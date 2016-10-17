6af30e0d5f811d98346743c9c397c2d4cd4ad08a
xunuosi <xunuosi@163.com>
2016-10-17 11:14:20 +0800

    Master Commit 使用注入工具ButterKnife并增加底部菜单互斥点击功能

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,20 +1,79 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.rb_newGoods)
+    RadioButton mRbNewGoods;
+    @BindView(R.id.rb_boutique)
+    RadioButton mRbBoutique;
+    @BindView(R.id.rb_category)
+    RadioButton mRbCategory;
+    @BindView(R.id.rb_cart)
+    RadioButton mRbCart;
+    @BindView(R.id.cart_count_textView)
+    TextView mCartCountTextView;
+    @BindView(R.id.rb_myCenter)
+    RadioButton mRbMyCenter;
+
+    int index;// 记录被点击RadioButton的下标索引
+    RadioButton[] mRadioButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        mRadioButtons = new RadioButton[5];
+        mRadioButtons[0] = mRbNewGoods;
+        mRadioButtons[1] = mRbBoutique;
+        mRadioButtons[2] = mRbCategory;
+        mRadioButtons[3] = mRbCart;
+        mRadioButtons[4] = mRbMyCenter;
     }
 
     public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.rb_newGoods:
+                index = 0;
+                break;
+            case R.id.rb_boutique:
+                index = 1;
+                break;
+            case R.id.rb_category:
+                index = 2;
+                break;
+            case R.id.rb_cart:
+                index = 3;
+                break;
+            case R.id.rb_myCenter:
+                index = 4;
+                break;
+        }
+        updateRadioButtonView();
+    }
 
+    private void updateRadioButtonView() {
+        L.i("xns", index + "");
+        for (int i = 0; i < mRadioButtons.length; i++) {
+            if (i == index) {
+                mRadioButtons[i].setChecked(true);
+                continue;
+            }
+            mRadioButtons[i].setChecked(false);
+        }
     }
 }

e3c57136c072bb5f1af2f037b2a34f6c4b4d491b
xunuosi <xunuosi@163.com>
2016-10-19 17:54:04 +0800

    修改NetDao的downloadNewGoods（）方法，以便复用。

--- app/src/main/java/cn/ucai/fulicenter/dao/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/dao/NetDao.java
@@ -9,13 +9,13 @@ package cn.ucai.fulicenter.dao;
         import cn.ucai.fulicenter.utils.L;
 
 public class NetDao {
-    public static void downloadNewGoods(Context mcontext, int page_id,
+    public static void downloadNewGoods(Context mcontext, int pageSize, int page_id,
                                         OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener) {
         OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(mcontext);
         utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
                 .addParam(I.GoodsDetails.KEY_CAT_ID,String.valueOf(I.CAT_ID))
                 .addParam(I.PAGE_ID,String.valueOf(page_id))
-                .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .addParam(I.PAGE_SIZE,String.valueOf(pageSize))
                 .targetClass(NewGoodsBean[].class)
                 .execute(listener);
     }

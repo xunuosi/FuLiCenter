e580fd89c10bc6ea36edd3d166d1b8529bde1423
xunuosi <xunuosi@163.com>
2016-10-19 17:08:24 +0800

    新建BoutiqueChildActivity并编写了布局

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/BoutiqueChildActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class BoutiqueChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boutique_child);
+    }
+}

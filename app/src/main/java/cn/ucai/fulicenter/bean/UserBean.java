51e34468a70c5913eb076115f1038260abc027ec
xunuosi <xunuosi@163.com>
2016-10-21 20:08:26 +0800

    新建登录成功后返回的用户信息制作UserBean类

---
+++ app/src/main/java/cn/ucai/fulicenter/bean/UserBean.java
@@ -0,0 +1,92 @@
+package cn.ucai.fulicenter.bean;
+
+
+public class UserBean {
+
+    /**
+     * muserName : xns1987
+     * muserNick : Elder
+     * mavatarId : 364
+     * mavatarPath : user_avatar
+     * mavatarSuffix : null
+     * mavatarType : 0
+     * mavatarLastUpdateTime : 1477049071315
+     */
+
+    private String muserName;
+    private String muserNick;
+    private int mavatarId;
+    private String mavatarPath;
+    private Object mavatarSuffix;
+    private int mavatarType;
+    private String mavatarLastUpdateTime;
+
+    public String getMuserName() {
+        return muserName;
+    }
+
+    public void setMuserName(String muserName) {
+        this.muserName = muserName;
+    }
+
+    public String getMuserNick() {
+        return muserNick;
+    }
+
+    public void setMuserNick(String muserNick) {
+        this.muserNick = muserNick;
+    }
+
+    public int getMavatarId() {
+        return mavatarId;
+    }
+
+    public void setMavatarId(int mavatarId) {
+        this.mavatarId = mavatarId;
+    }
+
+    public String getMavatarPath() {
+        return mavatarPath;
+    }
+
+    public void setMavatarPath(String mavatarPath) {
+        this.mavatarPath = mavatarPath;
+    }
+
+    public Object getMavatarSuffix() {
+        return mavatarSuffix;
+    }
+
+    public void setMavatarSuffix(Object mavatarSuffix) {
+        this.mavatarSuffix = mavatarSuffix;
+    }
+
+    public int getMavatarType() {
+        return mavatarType;
+    }
+
+    public void setMavatarType(int mavatarType) {
+        this.mavatarType = mavatarType;
+    }
+
+    public String getMavatarLastUpdateTime() {
+        return mavatarLastUpdateTime;
+    }
+
+    public void setMavatarLastUpdateTime(String mavatarLastUpdateTime) {
+        this.mavatarLastUpdateTime = mavatarLastUpdateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBean{" +
+                "muserName='" + muserName + '\'' +
+                ", muserNick='" + muserNick + '\'' +
+                ", mavatarId=" + mavatarId +
+                ", mavatarPath='" + mavatarPath + '\'' +
+                ", mavatarSuffix=" + mavatarSuffix +
+                ", mavatarType=" + mavatarType +
+                ", mavatarLastUpdateTime='" + mavatarLastUpdateTime + '\'' +
+                '}';
+    }
+}
